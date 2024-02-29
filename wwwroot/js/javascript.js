d30ba14326e34fa692236941c1ef315a5a7f95e6
maggyy666 <kczyz20@gmail.com>
2024-02-29 17:55:27 +0100

    Added New File [Login]

--- wwwroot/js/javascript.js
+++ wwwroot/js/javascript.js
@@ -2,3 +2,10 @@ function toggleMenu() {
     var navbarMenu = document.querySelector('.navbar-menu');
     navbarMenu.classList.toggle('active');
 }
+
+
+    document.getElementById("joinButton").addEventListener("click", function () {
+        var loginUrl = '@Url.Action("Login", "ControllerName")';
+    window.location.href = loginUrl;
+    });
+
