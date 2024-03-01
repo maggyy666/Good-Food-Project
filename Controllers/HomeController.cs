1a005de42bce39f3b3cfb0543481e9f9d32f348e
maggyy666 <kczyz20@gmail.com>
2024-03-01 15:21:31 +0100

    Created About / Contact
    Added them into HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,6 +36,14 @@ namespace GoodFoodProjectMVC.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
