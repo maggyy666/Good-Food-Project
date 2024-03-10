f070a68184fa130c6aed297aecb275f2bfd66019
maggyy666 <kczyz20@gmail.com>
2024-03-10 19:00:26 +0100

    Added connection string to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,42 @@
 ﻿using GoodFoodProjectMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Hosting.Server;
 
 namespace GoodFoodProjectMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private const string ConnectionString = "Server=localhost\\SQLEXPRESS01;Database=goodfood;Trusted_Connection=True;TrustServerCertificate=True;";
+
+
+        [HttpPost]
+        public IActionResult AddRecipe(Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                using(SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Recipes (Name, Description, ImagePath) VALUES (@Name, @Description, @ImagePath)";
+                    using (SqlCommand command = new SqlCommand(query,connection))
+                    {
+                        command.Parameters.AddWithValue("@Name", recipe.Name);
+                        command.Parameters.AddWithValue("@Description", recipe.Description);
+                        command.Parameters.AddWithValue("@ImagePath", recipe.ImagePath);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                return RedirectToAction("Recipes");
+            }
+            return View(recipe);
+        }
+
+
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
