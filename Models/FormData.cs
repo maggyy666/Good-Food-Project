74b6111d3ca9750098c7a0a4035ef3712430294d
maggyy666 <kczyz20@gmail.com>
2024-06-02 17:29:18 +0200

    Create FormData for MongoDB

---
+++ Models/FormData.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GoodFoodProjectMVC.Models
+{
+    public class FormData
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
