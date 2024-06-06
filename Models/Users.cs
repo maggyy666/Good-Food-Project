bbe897af40cf31dcadf5dd31f36a45f59015f813
maggyy666 <kczyz20@gmail.com>
2024-06-06 22:18:09 +0200

    Update users.cs

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,12 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace GoodFoodProjectMVC.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
-        public string Email { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
-        public DateTime Created_At { get; set; }
     }
 }
