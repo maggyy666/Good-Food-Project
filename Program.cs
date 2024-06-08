2d252b38f6059b0db6e56a7907c1463d0babe411
maggyy666 <kczyz20@gmail.com>
2024-06-08 21:32:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 using GoodFoodProjectMVC.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MongoDB.Driver;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSession();  // Add this line
+
+// Add session services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 // Configure MongoDB connection
 builder.Services.AddSingleton<IMongoClient>(s =>
@@ -13,18 +26,32 @@ builder.Services.AddSingleton<IMongoClient>(s =>
 
 builder.Services.AddSingleton<MongoDBService>();
 
+// Add authentication
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", options =>
+    {
+        options.LoginPath = "/Home/Login";
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-app.UseSession();  // Add this line
 
+// Use session
+app.UseSession();
+
+// Use authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
