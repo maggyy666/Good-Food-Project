faeb39ec43be717c7404369cb603855dd0c71486
maggyy666 <kczyz20@gmail.com>
2024-06-08 21:31:55 +0200

    Update Users

--- Models/Users.cs
+++ Models/Users.cs
@@ -8,7 +8,14 @@ namespace GoodFoodProjectMVC.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("username")]
         public string Username { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("password")]
         public string Password { get; set; }
     }
 }
